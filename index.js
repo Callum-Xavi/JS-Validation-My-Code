e0b8577b7d09462c3f29278ab4469913309f2c96
Callum/Xavi <callummander@gmail.com>
2021-11-12 16:02:28 +0000

    added some js validation for name and age field

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 const formSelection = document.querySelector("#example-form");
 
+// Handling errors with function
+function outputError(fieldID, errorMesage) {
+  console.log("running");
+  let errorPlaceholder = document.querySelector("#" + fieldID + "-error");
+  errorPlaceholder.innerText = errorMesage;
+
+}
+
+
 formSelection.addEventListener("submit", () => {
   console.log("hi");
   preventDefault();
 })
 
 let username = document.querySelector("#name-input");
-username.addEventListener("keyup", () => {
+username.addEventListener("keyup", (event) => {
   const name = username.value;
+  if (name.length > 10) {
+    outputError(event.target.id, "Error, username must be 10 characters or less");
+  } else {
+    outputError(event.target.id, "")
   }
+
 })
 
+// fix so that validation adhears when user uses up/down arrows on number box
 let userAge = document.querySelector("#age-input");
-userAge.addEventListener("keyup", () => {
-  const age = userAge.value;
+userAge.addEventListener("keyup", (event) => {
+  // age is now integer type
+  const age = Number(userAge.value);
+  if (age > 10 || age < -10) {
+    outputError(event.target.id, "Error, value must be between (and including) numbers: -10 and 10");
+  }
+
+  else {
+    outputError(event.target.id, "");
+  }
 
 })
 
